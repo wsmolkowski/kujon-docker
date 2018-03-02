efa344cb19a4f97928bc46de0f27cd51d4362dba
smola222 <wojciech.smolkowski@gmail.com>
2018-03-02 08:32:50 +0100

    add ipv6

--- firewall-ipv4ipv6.sh
+++ firewall-ipv4ipv6.sh
@@ -13,6 +13,7 @@
 ### END INIT INFO
 
 IPTABLES=/sbin/iptables
+IP6TABLES=/sbin/ip6tables
 
 echo " * flushing old rules"
 ${IPTABLES} --flush
@@ -20,17 +21,31 @@ ${IPTABLES} --delete-chain
 ${IPTABLES} --table nat --flush
 ${IPTABLES} --table nat --delete-chain
 
+${IP6TABLES} --flush
+${IP6TABLES} --delete-chain
+${IP6TABLES} --table nat --flush
+${IP6TABLES} --table nat --delete-chain
+
+
 echo " * setting default policies"
 ${IPTABLES} -P INPUT DROP
 ${IPTABLES} -P FORWARD DROP
 ${IPTABLES} -P OUTPUT ACCEPT
 
+${IP6TABLES} -P INPUT DROP
+${IP6TABLES} -P FORWARD DROP
+${IP6TABLES} -P OUTPUT ACCEPT
+
 echo " * allowing loopback devices"
 ${IPTABLES} -A INPUT -i lo -j ACCEPT
 ${IPTABLES} -A OUTPUT -o lo -j ACCEPT
-
 ${IPTABLES} -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
 ${IPTABLES} -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+${IP6TABLES} -A INPUT -i lo -j ACCEPT
+${IP6TABLES} -A OUTPUT -o lo -j ACCEPT
+${IP6TABLES} -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+${IP6TABLES} -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 ## BLOCK ABUSING IPs HERE ##
 #echo " * BLACKLIST"
@@ -39,6 +54,7 @@ ${IPTABLES} -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 echo " * allowing ssh on port 22"
 ${IPTABLES} -A INPUT -p tcp --dport 22  -m state --state NEW -j ACCEPT
+${IP6TABLES} -A INPUT -p tcp --dport 22  -m state --state NEW -j ACCEPT
 
 #echo " * allowing ftp on port 21"
 #${IPTABLES} -A INPUT -p tcp --dport 21  -m state --state NEW -j ACCEPT
@@ -51,13 +67,17 @@ ${IPTABLES} -A INPUT -p tcp --dport 22  -m state --state NEW -j ACCEPT
 
 echo " * allowing http on port 80"
 ${IPTABLES} -A INPUT -p tcp --dport 80  -m state --state NEW -j ACCEPT
+${IP6TABLES} -A INPUT -p tcp --dport 80  -m state --state NEW -j ACCEPT
 
 echo " * allowing https on port 443"
 ${IPTABLES} -A INPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
+${IP6TABLES} -A INPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
 
 echo " * allowing smtp on port 25 and 465"
 ${IPTABLES} -A INPUT -p tcp -m state --state NEW -m tcp --dport 25 -j ACCEPT
 ${IPTABLES} -A INPUT -p tcp -m state --state NEW -m tcp --dport 465 -j ACCEPT
+${IP6TABLES} -A INPUT -p tcp -m state --state NEW -m tcp --dport 25 -j ACCEPT
+${IP6TABLES} -A INPUT -p tcp -m state --state NEW -m tcp --dport 465 -j ACCEPT
 
 #echo " * allowing submission on port 587"
 #${IPTABLES} -A INPUT -p tcp -m state --state NEW -m tcp --dport 587 -j ACCEPT
@@ -65,6 +85,7 @@ ${IPTABLES} -A INPUT -p tcp -m state --state NEW -m tcp --dport 465 -j ACCEPT
 echo " * allowing imap on port 993"
 #${IPTABLES} -A INPUT -p tcp -m state --state NEW -m tcp --dport 143 -j ACCEPT
 ${IPTABLES} -A INPUT -p tcp -m state --state NEW -m tcp --dport 993 -j ACCEPT
+${IP6TABLES} -A INPUT -p tcp -m state --state NEW -m tcp --dport 993 -j ACCEPT
 
 #echo " * allowing pop on port 995 and 110"
 #${IPTABLES} -A INPUT -p tcp -m state --state NEW -m tcp --dport 995 -j ACCEPT
@@ -77,7 +98,10 @@ ${IPTABLES} -A INPUT -p tcp -m state --state NEW -m tcp --dport 993 -j ACCEPT
 
 echo " * allowing ping responses"
 ${IPTABLES} -A INPUT -p ICMP --icmp-type 8 -j ACCEPT
+${IP6TABLES} -A INPUT -p ICMP --icmp-type 8 -j ACCEPT
 
 # DROP everything else and Log it
 ${IPTABLES} -A INPUT -j LOG
 ${IPTABLES} -A INPUT -j DROP
+${IP6TABLES} -A INPUT -j LOG
+${IP6TABLES} -A INPUT -j DROP
